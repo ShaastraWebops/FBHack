13f311267c9161cee45ed06f0e7f68dabc044614
S.R.Manikandasriram <srmanikandasriram@gmail.com>
2012-09-17 18:38:53 +0530

    Likes app modified

--- likes/views.py
+++ likes/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template.context import Context, RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth.models import User
-import urllib, json
+import urllib, json, cgi
 from django.conf import settings
 from FBHack.likes.models import *
 
@@ -12,17 +12,23 @@ def likes(request):
     shares = request.POST.get("shares",0)
     pid = request.POST.get("pid",0)
     post = FBPosts.objects.get(id=pid)
-    users = FBUserProfile.objects.all().order_by('likes_used')[:likes]
+    users = FBUserProfile.objects.all().order_by('likes_used')
+    target = urllib.urlopen('https://graph.facebook.com/' + post.post_id + '/likes?fields=id&limit=1000').read()
+    response = json.loads(target)['data']
+    liked_by = [str(x['id']) for x in response]
     cnt = 0
     for user in users :
-      cnt = cnt +1
-      #response = urllib.urlopen('https://graph.facebook.com/' + post.post_id + '/likes?access_token='+ user.access_token).read()
-      #if response == 'true' :
-	#cnt = cnt + 1
-	# user.likes_used = user.likes_used + 1
-	# user.save()
-    # post.likes_given = post.likes_given + cnt
-    # post.save()
-    assert False
+      if user.facebook_id not in liked_by :
+	response = urllib.urlopen('https://graph.facebook.com/' + post.post_id + '/likes?access_token='+ user.access_token).read()
+	if response == 'true' :
+	  cnt = cnt + 1
+	  user.likes_used = user.likes_used + 1
+	  user.save()
+	else :
+	  user.active = False
+	  user.save()
+    post.likes_given = post.likes_given + cnt
+    post.save()
+    #assert False
     return HttpResponseRedirect(settings.SITE_URL)  
   return Http404()
