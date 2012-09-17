933889b46c7b094b03d75281b4d199d3eb4003fb
Shaastra <webops@shaastra.org>
2012-09-17 07:58:06 -0500

    Hosting Edits
    These are the changes made to host the app on server

---
+++ likes/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from users.models import FBUserProfile
+# Create your models here.
+
+class FBPosts(models.Model):
+  desc = models.CharField(max_length = 500)
+  post_id = models.CharField(max_length = 50)
+  link = models.CharField(max_length = 500)
+  likes = models.IntegerField(default=0)
+  shares = models.IntegerField(default=0)
+  likes_given = models.IntegerField(default=0)
+  shares_given = models.IntegerField(default=0) 
+
+class likes(models.Model):
+  like_id = models.CharField(max_length = 50)
+  postId = models.ForeignKey(FBPosts, related_name="post")
+  user_id = models.ForeignKey(FBUserProfile)
